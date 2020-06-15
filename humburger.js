368f25e99bfe99f57914b19d3a3f16c2e279ecba
unknown <ghazishabo3@gmail.com>
2020-06-15 16:36:41 +0200

    first burger

---
+++ humburger.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector(".humburger");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click" , () => {
+    navbar.classList.toggle("change")
+    
+})
